d38b275073952187e4bbb1b535d1e954a06957ae
lito00356 <tomasz.liksza@gmail.com>
2026-02-18 12:02:05 +0100

    Created pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./style/index.css";
 import App from "./App.jsx";
+import { BrowserRouter, Route } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="profile" element={"/profile"} />
+          <Route path="collection" element={"/collection"} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
