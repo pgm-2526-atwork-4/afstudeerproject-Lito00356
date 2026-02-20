07a98e9d4ec35c0b35a3c01aea90cc59b93f4e34
lito00356 <tomasz.liksza@gmail.com>
2026-02-20 14:33:01 +0100

    logout is working

--- src/app/pages/Collection/Collection.jsx
+++ src/app/pages/Collection/Collection.jsx
@@ -1,9 +1,11 @@
+import { logout } from "@core/modules/auth/api.auth";
 import React from "react";
 
 const Collection = () => {
   return (
     <>
       <h1>Collection</h1>
+      <button onClick={() => logout()}>Logout</button>
     </>
   );
 };
