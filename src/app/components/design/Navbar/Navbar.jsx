9acc28b0d48b280e7a740ac73c787a46f2b1e756
lito00356 <tomasz.liksza@gmail.com>
2026-02-22 23:23:19 +0100

    navbar component

--- src/app/components/design/Navbar/Navbar.jsx
+++ src/app/components/design/Navbar/Navbar.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Link } from "react-router";
+import "../../../../style/theme.css";
+import "./Navbar.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="container-70 navbar">
+      <span className="">
+        <img className="" src="https://placehold.co/25x25" alt="" />
+        <h2 className="">RoomCraft</h2>
+      </span>
+      <ul className="">
+        <li className="">
+          <Link className="" to="/login">
+            Login
+          </Link>
+        </li>
+        <li className="">
+          <Link className="" to="/register">
+            Get started
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
