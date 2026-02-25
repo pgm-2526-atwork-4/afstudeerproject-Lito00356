9af1ed395fec1f797921bc08e75f10d61d1fcf8f
lito00356 <tomasz.liksza@gmail.com>
2026-02-25 14:40:42 +0100

    added hook to keep screen full size

--- src/app/pages/Perspective/Perspective.jsx
+++ src/app/pages/Perspective/Perspective.jsx
@@ -1,20 +1,89 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useThree } from "@react-three/fiber";
 import "@style/theme.css";
 import "./perspective.css";
-import React from "react";
-import { OrbitControls } from "@react-three/drei";
+import React, { useEffect, useState } from "react";
+import { OrbitControls, Wireframe } from "@react-three/drei";
 
-const Perspective = () => {
+function Scene() {
+  const { setSize, size } = useThree();
+
+  useEffect(() => {
+    const handleResize = () => {
+      setSize(window.innerWidth, window.innerHeight);
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, [setSize]);
+
   return (
     <>
+      <mesh scale={[5, 5, 0]} rotation-x={Math.PI * 0.5} position={[0, -2, 0]}>
+        <planeGeometry />
+        <meshBasicMaterial color={"red"} />
+      </mesh>
+    </>
+  );
+}
+
+const Perspective = () => {
+  const [room, setRoom] = useState({
+    objects: [],
+  });
+
+  function addCube() {
+    setRoom((prev) => ({
+      ...prev,
+      objects: [
+        ...prev.objects,
+        {
+          id: crypto.randomUUID(),
+          type: "box",
+          position: [0, 0, 0],
+          color: "red",
+        },
+      ],
+    }));
+  }
+
+  function saveCube() {
+    console.log("hello");
+  }
+
+  return (
+    <div className="canvas-page">
       <Canvas className="canvas">
-        <mesh scale={[2, 4, 4]}>
-          <boxGeometry />
-          <meshBasicMaterial color="yellow" />
+        <Scene />
+        <mesh scale={[5, 0, 5]}>
+          <plane />
         </mesh>
+        {room.objects.map((obj) => {
+          switch (obj.type) {
+            case "box":
+              return (
+                <mesh scale={[2, 4, 4]} key={obj.id}>
+                  <boxGeometry />
+                  <meshBasicMaterial color={obj.color} />
+                  <Wireframe fillMix={0} stroke={"black"} thickness={0.02} />
+                </mesh>
+              );
+            case "sphere":
+              return <Sphere key={obj.id} {...obj} />;
+          }
+        })}
         <OrbitControls />
       </Canvas>
-    </>
+      <div className="ui-overlay">
+        <button className="add-block-btn" onClick={() => addCube()}>
+          + Voeg blokje toe
+        </button>
+        <button className="save-btn" onClick={() => saveCube()}>
+          Sla blokje op
+        </button>
+      </div>
+    </div>
   );
 };
 
