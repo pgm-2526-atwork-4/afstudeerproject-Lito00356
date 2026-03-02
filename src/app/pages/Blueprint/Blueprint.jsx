5a3db216efecfc74f0c7164b55c1f383ab6ec362
lito00356 <tomasz.liksza@gmail.com>
2026-03-02 12:29:25 +0100

    stared making gird layout in blueprint

--- src/app/pages/Blueprint/Blueprint.jsx
+++ src/app/pages/Blueprint/Blueprint.jsx
@@ -14,6 +14,62 @@ const Blueprint = () => {
     queryFn: () => getProjectById(projectId),
   });
 
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const context = canvas.getContext("2d");
+
+    const syncCanvasSize = () => {
+      const rectangle = canvas.getBoundingClientRect();
+
+      canvas.width = rectangle.width;
+      canvas.height = rectangle.height;
+
+      console.log(`Canvas size: ${canvas.width}x${canvas.height}`);
+    };
+
+    syncCanvasSize();
+
+    const drawVertivalGrid = () => {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+
+      context.strokeStyle = "#e0e0e0";
+      context.lineWidth = 1;
+
+      for (let x = 0; x <= canvas.width; x += 20) {
+        context.beginPath();
+        context.moveTo(x, 0);
+        context.lineTo(x, canvas.height);
+        context.stroke();
+      }
+    };
+
+    const drawHorizontalGrid = () => {
+      context.strokeStyle = "#e0e0e0";
+      context.lineWidth = 1;
+
+      for (let y = 0; y <= canvas.height; y += 20) {
+        context.beginPath();
+        context.moveTo(0, y);
+        context.lineTo(canvas.width, y);
+        context.stroke();
+      }
+    };
+
+    drawVertivalGrid();
+    drawHorizontalGrid();
+
+    const handleResize = () => {
+      syncCanvasSize();
+      drawGrid();
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div className="blueprint-fullscreen">
       <div className="canvas-container">
