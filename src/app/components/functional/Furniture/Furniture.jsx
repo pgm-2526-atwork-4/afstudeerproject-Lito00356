58bdb0c1aa4996b2a340d3d5265b659928df3091
lito00356 <tomasz.liksza@gmail.com>
2026-03-03 23:07:08 +0100

    removed a bit of code

--- src/app/components/functional/Furniture/Furniture.jsx
+++ src/app/components/functional/Furniture/Furniture.jsx
@@ -4,13 +4,6 @@ import React from "react";
 const Furniture = ({ position = [0, 0, 0], scale = [1, 1, 1], rotation = [0, 0, 0] }) => {
   const { scene } = useGLTF("/models/sofa.gltf");
 
-  scene.traverse((child) => {
-    if (child.material) {
-      child.material.map = null;
-      child.material.visible = true;
-    }
-  });
-
   return (
     <>
       <primitive object={scene} position={position} scale={scale} rotation={rotation} />;
