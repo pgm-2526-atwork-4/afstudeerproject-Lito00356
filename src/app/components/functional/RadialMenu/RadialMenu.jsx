5b82a6dc13ef116c3c8aae52576118a7b20b16ee
lito00356 <tomasz.liksza@gmail.com>
2026-03-07 21:17:47 +0100

    animation for radial menu made

--- src/app/components/functional/RadialMenu/RadialMenu.jsx
+++ src/app/components/functional/RadialMenu/RadialMenu.jsx
@@ -5,7 +5,7 @@ import React, { useState } from "react";
 
 const RadialMenu = ({ furnitureId }) => {
   const [isExpanded, setIsExpanded] = useState(false);
-  const colors = ["red", "green", "blue"];
+  const colors = ["red", "green", "blue", "orange", "purple", "pink"];
 
   const changeFurnitureColor = (furnitureId, color) => {
     console.log(color);
@@ -15,21 +15,24 @@ const RadialMenu = ({ furnitureId }) => {
     <Html position={[0, 1, 0]} wrapperClass="container" distanceFactor={5}>
       <div className={`radial-menu ${isExpanded ? "expanded" : ""}`}>
         <div className="main-btn" onClick={() => setIsExpanded(!isExpanded)}>
-          {isExpanded ? <X /> : <Palette />}
+          {isExpanded ? <X size={16} /> : <Palette size={16} />}
         </div>
 
-        {isExpanded &&
-          colors.map((color, i) => (
-            <div
-              key={color}
-              className="menu-item"
-              style={{ "--angle": `${i * 90}deg`, "--color": color }}
-              onClick={() => changeFurnitureColor(furnitureId, color)}
-            ></div>
-          ))}
+        {colors.map((color, i) => (
+          <div
+            key={color}
+            className="menu-item"
+            style={{
+              "--angle": `${i * 60}deg`,
+              "--color": color,
+              "--delay": `${i * 60}ms`,
+            }}
+            onClick={() => changeFurnitureColor(furnitureId, color)}
+          />
+        ))}
 
         <div className="material">
-          <BrickWall />
+          <BrickWall size={16} />
         </div>
       </div>
     </Html>
