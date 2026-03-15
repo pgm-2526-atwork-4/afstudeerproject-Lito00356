6cb080d31371a58b6dd884aadf2fa8a6a9d8d49d
lito00356 <tomasz.liksza@gmail.com>
2026-03-16 00:31:10 +0100

    Made the cube bigger

--- src/app/components/design/Button/CameraViewChanger/CameraViewChanger.jsx
+++ src/app/components/design/Button/CameraViewChanger/CameraViewChanger.jsx
@@ -5,14 +5,13 @@ const CameraViewChanger = ({ isTopView, onEnable, onDisable }) => {
   return (
     <button className={`topview-btn ${isTopView ? "topview-btn--active" : ""}`} onClick={isTopView ? onDisable : onEnable}>
       <div className="topview-btn__cube">
-        <div className="topview-btn__face topview-btn__face--front" />
+        <div className="topview-btn__face topview-btn__face--front">3D</div>
         <div className="topview-btn__face topview-btn__face--back" />
         <div className="topview-btn__face topview-btn__face--left" />
         <div className="topview-btn__face topview-btn__face--right" />
-        <div className="topview-btn__face topview-btn__face--top" />
+        <div className="topview-btn__face topview-btn__face--top">Top</div>
         <div className="topview-btn__face topview-btn__face--bottom" />
       </div>
-      {/* <span>{isTopView ? "3D View" : "Top View"}</span> */}
     </button>
   );
 };
