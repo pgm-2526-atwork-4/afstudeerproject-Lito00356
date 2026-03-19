9e3d071aeeadde50c96cb7f0e4c0d7d1427aa1d9
lito00356 <tomasz.liksza@gmail.com>
2026-03-19 15:32:32 +0100

    index for edgefunction | mailing

--- supabase/functions/send-email/index.ts
+++ supabase/functions/send-email/index.ts
@@ -1,29 +1,61 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
+// @ts-ignore
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
+// @ts-ignore
+const RESEND_API_KEY = Deno.env.get("RESEND_API_KEY")
+const EMAILS = "https://api.resend.com/emails"
 
-// Setup type definitions for built-in Supabase Runtime APIs
-import "@supabase/functions-js/edge-runtime.d.ts";
+serve(async (req) => {
+  if (req.method === "OPTIONS") {
+    return new Response("ok", {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "POST",
+        "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
+      },
+    });
+  }
 
-console.log("Hello from Functions!");
+  try{
+const { to, cc, subject, html, attachments} = await req.json();
 
-Deno.serve(async (req) => {
-  const { name } = await req.json();
-  const data = {
-    message: `Hello ${name}!`,
-  };
+const formattedAttachments = attachments?.map((att) => ({
+  filename: att.filename,
+  content: att.content,
+}));
 
-  return new Response(JSON.stringify(data), { headers: { "Content-Type": "application/json" } });
+const res = await fetch(EMAILS, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${RESEND_API_KEY}`,
+  },
+  body: JSON.stringify({
+    from: "RoomCraft <noreply@mydomain.com>",
+    to,
+    cc: cc || undefined,
+    subject,
+    html,
+    attachments: formattedAttachments || undefined,
+  }),
 });
 
-/* To invoke locally:
+const data = await res.json()
 
-  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
-  2. Make an HTTP request:
+if (!res.ok){
+    return new Response(JSON.stringify({error: data}), {
+      status: 400,
+      headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
+  });
+}
 
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/send-email' \
-    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-    --header 'Content-Type: application/json' \
-    --data '{"name":"Functions"}'
-
-*/
+return new Response(JSON.stringify(data), {
+  status: 200,
+  headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
+})
+  } catch (err){
+    return new Response(JSON.stringify({ error: err.message }), {
+      status: 500,
+      headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
+    });
+  }
+})
