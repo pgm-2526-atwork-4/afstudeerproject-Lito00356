d008844323027d2f92db9af76aa6ed3854a01357
lito00356 <tomasz.liksza@gmail.com>
2026-03-21 21:17:04 +0100

    adjusted top view rotationg angle

--- src/app/components/functional/CameraController/CameraController.jsx
+++ src/app/components/functional/CameraController/CameraController.jsx
@@ -7,12 +7,13 @@ const CameraController = ({ isTopView }) => {
   useEffect(() => {
     if (isTopView) {
       camera.position.set(0, 30, 0);
-      camera.rotation.set(Math.PI / 2, 0, 0);
+      camera.up.set(0, 90, 1);
       // eslint-disable-next-line
       camera.fov = 10;
       camera.updateProjectionMatrix();
     } else {
       camera.position.set(10, 6, 10);
+      camera.up.set(0, 115, 1);
       camera.fov = 50;
       camera.updateProjectionMatrix();
     }
