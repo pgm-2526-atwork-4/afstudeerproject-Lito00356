a19f59fcda7c69b94577e168552c1a1164ecb8a8
lito00356 <tomasz.liksza@gmail.com>
2026-03-22 10:45:44 +0100

    created a toast hook

---
+++ src/core/hooks/useToast.js
@@ -0,0 +1,23 @@
+import { Children, createContext, useCallback, useContext, useState } from "react";
+
+const ToastContext = createContext();
+
+export const useToast = () => useContext(ToastContext);
+
+export const ToastProvider = ({ children }) => {
+  const [toasts, setToasts] = useState([]);
+
+  const addToast = useCallback((message, type = "succes") => {
+    const id = Date.now();
+    setToasts((prev) => [...prev, { id, message, type }]);
+    setTimeout(() => {
+      setToasts((prev) => prev.filter((toast) => toast.id !== id));
+    }, 3000);
+  }, []);
+
+  const removeToast = useCallback((id) => {
+    setToasts((prev) => prev.filter((toast) => toast.id !== id));
+  }, []);
+
+  return <ToastContext.Provider value={{ addToast }}>{children}</ToastContext.Provider>;
+};
